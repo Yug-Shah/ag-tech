1862fc5d6189b2e9764a328b16b6807ba348063b
Yug Shah <yas689@uregina.ca>
2022-06-21 03:50:50 -0600

    Videos on Education Hub

--- src/app/components/tab3/tab3.page.ts
+++ src/app/components/tab3/tab3.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-
-  constructor() {}
-
+export class Tab3Page implements OnInit{
+  trustedVideoUrlArray: SafeResourceUrl[]=[];
+  urlsArray = [
+    {
+      link: '../../../assets/Videos/100.mp4'
+    },
+    {
+      link: '../../../assets/Videos/200.mp4'
+    }
+  ];
+  constructor(public navCtrl: NavController,
+              private domSanitizer: DomSanitizer) {}
+  ngOnInit(): void {
+    for(const item of this.urlsArray){
+      this.trustedVideoUrlArray.push(this.domSanitizer.bypassSecurityTrustResourceUrl(item.link));
+    }
+  }
 }
+
+
+
+
+
+
+
