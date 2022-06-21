eb98bb47f0e297872ac5e58b05b70ec2a956188d
Yug Shah <yas689@uregina.ca>
2022-06-21 05:22:01 -0600

    Different Pages for Buy and Sell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {IntroductionComponent} from './components/introduction/introduction.component';
 import {LoginComponent} from './components/login/login.component';
+import {BuyComponent} from './components/buy/buy.component';
+import {SellComponent} from './components/sell/sell.component';
 
 const routes: Routes = [
   {
@@ -20,6 +22,14 @@ const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./components/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'buy',
+    component: BuyComponent
+  },
+  {
+    path: 'sell',
+    component: SellComponent
   }
 ];
 @NgModule({
